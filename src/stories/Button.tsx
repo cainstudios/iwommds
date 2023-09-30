02e4bb13d1898d7b9f48e458f13675f7ee3e8388
Tim Cain <cainstudios@gmail.com>
2023-09-30 18:43:28 -0500

    add some intgration of tailwinds and storybook

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './button.css';
+import React from "react";
+import "./button.css";
 
 interface ButtonProps {
   /**
@@ -13,7 +13,7 @@ interface ButtonProps {
   /**
    * How large should the button be?
    */
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
   /**
    * Button contents
    */
@@ -29,16 +29,24 @@ interface ButtonProps {
  */
 export const Button = ({
   primary = false,
-  size = 'medium',
+  size = "medium",
   backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={[
+        "outline",
+        "outline-pink-500",
+        "storybook-button",
+        `storybook-button--${size}`,
+        mode,
+      ].join(" ")}
       style={{ backgroundColor }}
       {...props}
     >
@@ -46,3 +54,58 @@ export const Button = ({
     </button>
   );
 };
+
+/** 
+ * Secondary UI component for user interaction
+ * // ./src/stories/Button.js
+import React, { useMemo } from "react";
+
+const getSizeClasses = (size: string) => {
+  switch (size) {
+    case "small": {
+      return "px-4 py-2.5";
+    }
+    case "large": {
+      return "px-6 py-3";
+    }
+    default: {
+      return "px-5 py-2.5";
+    }
+  }
+};
+
+const getModeClasses = (isPrimary: boolean) =>
+  isPrimary
+    ? "text-white bg-pink-600 border-pink-600 dark:bg-pink-700 dark:border-pink-700"
+    : "text-slate-700 bg-transparent border-slate-700 dark:text-white dark:border-white";
+
+const BASE_BUTTON_CLASSES =
+  "cursor-pointer rounded-full border-2 font-bold leading-none inline-block";
+
+/**
+ * Primary UI component for user interaction
+ */
+/*export const Button = ({
+  primary = false,
+  size = "medium",
+  label = "",
+  ...props
+}) => {
+  const computedClasses = useMemo(() => {
+    const modeClass = getModeClasses(primary);
+    const sizeClass = getSizeClasses(size);
+
+    return [modeClass, sizeClass].join(" ");
+  }, [primary, size]);
+
+  return (
+    <button
+      type="button"
+      className={`${BASE_BUTTON_CLASSES} ${computedClasses}`}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+*/
